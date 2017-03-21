df3dfab8407538717e2a7077f7931f79b3cbfe4f
student5 <student3@jh-mba158-10.local>
2017-03-21 10:02:36 -0500

    Used CLGeocoder

--- MobileMapper/ViewController.swift
+++ MobileMapper/ViewController.swift
@@ -11,17 +11,33 @@ import MapKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
+    
+    let herseyAnnotation = MKPointAnnotation()
+    let address = "Springfield"
+    let geocoder = CLGeocoder()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let herseyAnnotation = MKPointAnnotation()
         let latitude: Double = 42.102332924
         let longitude: Double = -87.955667844
         let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         herseyAnnotation.coordinate = coordinate
         herseyAnnotation.title = "John Hersey High School"
         mapView.addAnnotation(herseyAnnotation)
+        
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            print(placemarks?.count)
+            for place in placemarks! {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = (place.location?.coordinate)!
+                annotation.title = place.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
     }
+    
+    
 
 }
 
